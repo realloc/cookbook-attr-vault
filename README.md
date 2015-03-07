d931eed3862fcadf4e0f98687448e792a8d122d0
Stanislav Bogatyrev <realloc@realloc.spb.ru>
2015-03-08 02:42:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# attr-vault
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'attr-vault'
+maintainer       'Stanislav Bogatyrev'
+maintainer_email 'realloc@realloc.spb.ru'
+license          'apache2'
+description      'Installs/Configures attr-vault'
+long_description 'Installs/Configures attr-vault'
+version          '0.1.0'
+
